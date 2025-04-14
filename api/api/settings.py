fd5587dd1ad805a3c97be13e1b721bceca48966f
scmc24 <manuelsokoudjou@gmail.com>
2025-04-14 08:25:32 +0200

    configuration of external network

--- api/api/settings.py
+++ api/api/settings.py
@@ -134,6 +134,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
        #'rest_framework.permissions.IsAuthenticated',
    ],
+   'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
    'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework.authentication.BasicAuthentication',
        'rest_framework.authentication.SessionAuthentication',
